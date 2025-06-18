f97bc396ba3cf7c5bccd779ecb688159dcfa90a1
TheMLG <mlgoswami1801@gmail.com>
2025-06-18 12:33:14 +0530

    refactor: remove base path configuration from Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/DCWeb_Fullstack_Frontend/",
   plugins: [react()],
   server: {
     proxy: {
